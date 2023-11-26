332242b16e59bc6d83b5676c4b5bcb2fac24a5a3
Noor ul ain Ibrahim <noorulainibrahim75@gmail.com>
2023-11-26 15:57:38 +0400

    Auth service deployment done on kubernetes cluster

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM python:3.10-slim-bullseye
 
-RUN apt-get-update \
+RUN apt-get update \
     && apt-get install -y --no-install-recommends --no-install-suggests \
-    build-essential default-libmysqlclient-dev \
+       build-essential default-libmysqlclient-dev pkg-config \
     && pip install --no-cache-dir --upgrade pip
+
+RUN pip install PyJWT flask flask-mysqldb python-dotenv
 
 WORKDIR /app
 COPY ./requirements.txt /app
-RUN pip install --no-cache-dir --requirements /app/requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
 COPY . /app
 EXPOSE 5000
 
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-import jwt, os, datetime
+import jwt
+import os
+import datetime
 from flask import Flask, request
+
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 
@@ -7,50 +10,57 @@ from dotenv import load_dotenv
 load_dotenv()
 
 server = Flask(__name__)
-mysql = MySQL(server)
 
+# Set MySQL configuration using Flask's config
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 
+# Initialize MySQL
+mysql = MySQL(server)
+
 @server.route("/login", methods=["POST"])
-def login(request):
+def login():
     auth = request.authorization
-    print("this is auth header", auth)
-    if not auth:
-        return "missing credentails", 401
-    cur = mysql.connection.cursor()
-    res = cur.execute("SELECT email, password FROM users WHERE email=%s", (auth.username,))
-    if res > 0:
-        user_row = cur.fetchone()
-        email = user_row[0]
-        password = user_row[1]
-        if auth.username != email and auth.password != password:
-            return "Invalid credentails", 403
-        else:
-            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
-    else:
-        return "Invalid credentials", 401
-    
+
+    if not auth or not auth.username or not auth.password:
+        return "Missing or invalid credentials", 401
+
+    try:
+        with mysql.connection.cursor() as cur:
+            res = cur.execute("SELECT email, password FROM users WHERE email=%s", (auth.username,))
+            if res > 0:
+                user_row = cur.fetchone()
+                email, password = user_row[0], user_row[1]
+                if auth.username == email and auth.password == password:
+                    return create_jwt(auth.username, os.environ.get("JWT_SECRET"), True)
+                else:
+                    return "Invalid credentials", 403
+            else:
+                return "Invalid credentials", 401
+    except Exception as e:
+        return f"Error: {str(e)}", 500
+
 @server.route("/validate", methods=["POST"])
-def validate(request):
-    encoded_jwt = request.headers["authorization"]
+def validate():
+    encoded_jwt = request.headers.get("authorization")
+
     if not encoded_jwt:
         return "Missing Credentials", 401
+
     encoded_jwt = encoded_jwt.split(" ")[1]
+
     try:
         decoded = jwt.decode(encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"])
+        return decoded, 200
+    except jwt.ExpiredSignatureError:
+        return "Token has expired", 401
+    except jwt.InvalidTokenError:
+        return "Invalid token", 403
 
-    except:
-        return "Not Authorized", 403
-    
-    return decoded, 200
-
-
-
-def createJWT(username, secret, authz):
+def create_jwt(username, secret, authz):
     return jwt.encode(
         {
             "username": username,
